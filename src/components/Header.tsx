b306279f6291213b8290449d9a2b889c8c5e61d5
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-10-11 16:13:58 -0600

    Created Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
+import { IoCarSport } from "react-icons/io5";
+import { IoIosSettings } from "react-icons/io";
+
 function Header() {
   return (
-    <>
-      <p>header</p>
-    </>
+    <header className="flex items-center justify-between bg-blue-600 px-8 py-1">
+      <div className="flex items-center gap-2 font-bold">
+        <IoCarSport size={40} /> Drive Check
+      </div>
+      <div className="text-lg font-bold uppercase">Maintenence tracker</div>
+      <IoIosSettings size={30} />
+    </header>
   );
 }
 
