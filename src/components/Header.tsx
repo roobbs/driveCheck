f87762fa179fe1ac982c1f3c47174fe73f4e3fe6
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-10-12 21:01:23 -0600

    Modified Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,17 @@ import { IoIosSettings } from "react-icons/io";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between bg-blue-600 px-8 py-1">
-      <div className="flex items-center gap-2 font-bold">
+    <header className="flex items-center justify-between bg-blue-600 px-8 py-3">
+      <div className="flex cursor-pointer items-center gap-2 rounded-xl border border-transparent bg-white px-2 font-bold text-blue-800 transition-colors hover:border-white hover:bg-transparent hover:text-white">
         <IoCarSport size={40} /> Drive Check
       </div>
       <div className="text-lg font-bold uppercase">Maintenence tracker</div>
-      <IoIosSettings size={30} />
+      <div>
+        <IoIosSettings
+          size={30}
+          className="rounded-full transition-colors hover:bg-white hover:text-blue-600"
+        />
+      </div>
     </header>
   );
 }
