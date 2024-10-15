ceeaaccda1db945830789cb055e4ee7da4b5984e
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-10-14 22:05:12 -0600

    Created Summary.tsx

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,7 +2,7 @@ import Summary from "./Summary";
 
 export default function Content() {
   return (
-    <main className="flex flex-1 flex-col gap-8 bg-gray-900 p-4">
+    <main className="flex flex-1 flex-col gap-8 bg-gray-800 p-4">
       <Summary />
       <Summary />
     </main>
---
+++ src/components/Summary.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { IoIosArrowDropdown, IoIosArrowDropup } from "react-icons/io";
+
+export default function Summary() {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <section className="rounded-lg bg-gray-900 p-3 py-6">
+      <div className="flex justify-around">
+        <div className="font-bold text-yellow-300">Vehicle Summary</div>
+        <div
+          onClick={() => setOpen(!open)}
+          className="transition hover:text-yellow-300"
+        >
+          {!open && <IoIosArrowDropdown size={25} />}
+          {open && <IoIosArrowDropup size={25} />}
+        </div>
+      </div>
+      {open && (
+        <div className="flex justify-around gap-8">
+          <div>
+            <div className="text-blue-400">Model</div>
+            <div className="text-xl font-bold"> Ford Ranger</div>
+          </div>
+          <div>
+            <div className="text-blue-400">Year</div>
+            <div className="text-xl font-bold">1995</div>
+          </div>
+          <div>
+            <div className="text-blue-400">Mileage</div>
+            <div className="text-xl font-bold">221,750</div>
+          </div>
+          <div>
+            <div className="text-blue-400">Last Service</div>
+            <div className="text-xl font-bold">23 ago 2024</div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+}
