50633bda3b67960531baf89ea9c05f5dcd5fa17e
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-10-18 22:12:28 -0600

    Added firebase authentication configuration

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // import { getAnalytics } from "firebase/analytics"; only for analytics
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA1AN6wbrVW-OiA7T5OBNqiej0D1SRbDiM",
   authDomain: "drivecheck-87bfc.firebaseapp.com",
@@ -17,6 +12,7 @@ const firebaseConfig = {
   measurementId: "G-4BD02RVJE3",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
 // const analytics = getAnalytics(app);
