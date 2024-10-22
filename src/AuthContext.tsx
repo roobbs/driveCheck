30c70313885ae2af0a980c2941d5edcf4b61092b
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-10-21 22:52:36 -0600

    Created basic AuthContext.tsx

---
+++ src/AuthContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, useState } from "react";
+import { User, AuthProviderProps } from "../utils/Interfaces";
+
+interface AuthContextType {
+  user: User | null;
+  addUser: (user: User) => void;
+  logOutUser: () => void;
+}
+
+export const AuthContext = createContext<AuthContextType>({
+  user: null,
+  addUser: () => {},
+  logOutUser: () => {},
+});
+
+export default function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const addUser = (user: User) => {
+    setUser(user);
+  };
+
+  const logOutUser = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        addUser,
+        logOutUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
