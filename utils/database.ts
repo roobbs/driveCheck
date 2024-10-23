8b1b11ec170f46da53a90eaf4a588710bd22525f
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-10-23 15:32:42 -0600

    Modified signInWithGoogle function

--- utils/database.ts
+++ utils/database.ts
@@ -1,9 +1,22 @@
 import { signInWithPopup, signOut } from "firebase/auth";
 import { auth, googleProvider } from "../config/firebase";
+import { User } from "./Interfaces";
 
 export const signInWithGoogle = async () => {
   try {
-    await signInWithPopup(auth, googleProvider);
+    const result = await signInWithPopup(auth, googleProvider);
+    console.log(result);
+    const user = result.user;
+    console.log(user);
+
+    const mappedUser: User = {
+      uid: user.uid,
+      email: user.email || "",
+      displayName: user.displayName || "",
+      photoURL: user.photoURL || "",
+    };
+
+    return mappedUser;
   } catch (error) {
     console.log(error);
   }
