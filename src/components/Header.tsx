39b9e6906107d6cce5e6c45dfb7cb397c9e55a18
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-10-25 20:16:38 -0600

    Change H.eader style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,19 @@
 import { IoCarSport } from "react-icons/io5";
-import { IoIosSettings } from "react-icons/io";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between bg-blue-600 px-8 py-3">
-      <div className="flex cursor-pointer items-center gap-2 rounded-xl border border-transparent bg-white px-2 font-bold text-blue-800 transition-colors hover:border-white hover:bg-transparent hover:text-white">
-        <IoCarSport size={40} /> Drive Check
+    <header className="flex items-center justify-between bg-blue-800 px-8 py-3">
+      <div className="flex items-center gap-4">
+        <div className="flex cursor-pointer items-center gap-2 rounded-xl border border-transparent bg-white px-2 font-bold text-blue-800 transition-colors hover:border-white hover:bg-transparent hover:text-white">
+          <IoCarSport size={40} /> Drive Check
+        </div>
+        <div className="text-lg font-bold uppercase">Maintenence tracker</div>
       </div>
-      <div className="text-lg font-bold uppercase">Maintenence tracker</div>
       <div>
-        <IoIosSettings
+        <GiHamburgerMenu
           size={30}
-          className="rounded-full transition-colors hover:bg-white hover:text-blue-600"
+          className="transition-colors hover:text-yellow-300"
         />
       </div>
     </header>
