751c8b1e799cadb060151cdded91fc15870b8f23
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-11-04 17:41:23 -0600

    Added function to change language

--- src/components/auth/AuthContext.tsx
+++ src/components/auth/AuthContext.tsx
@@ -5,16 +5,21 @@ interface AuthContextType {
   user: User | null;
   addUser: (user: User) => void;
   deleteUserFromContext: () => void;
+  language: string;
+  changeLanguage: (language: string) => void;
 }
 
 export const AuthContext = createContext<AuthContextType>({
   user: null,
   addUser: () => {},
   deleteUserFromContext: () => {},
+  language: "eng",
+  changeLanguage: () => {},
 });
 
 export default function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User | null>(null);
+  const [language, setLanguage] = useState<string>("esp");
 
   const addUser = (user: User) => {
     setUser(user);
@@ -24,12 +29,22 @@ export default function AuthProvider({ children }: AuthProviderProps) {
     setUser(null);
   };
 
+  const changeLanguage = (newLanguage: string) => {
+    if (newLanguage === "esp") {
+      setLanguage("esp");
+    } else if (newLanguage === "eng") {
+      setLanguage("eng");
+    }
+  };
+
   return (
     <AuthContext.Provider
       value={{
         user,
         addUser,
         deleteUserFromContext,
+        language,
+        changeLanguage,
       }}
     >
       {children}
