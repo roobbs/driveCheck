74bcc7511b3ee35058c1ae518ecfe777c359f83a
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-11-05 16:18:17 -0600

    Translated Index.tsx header

--- src/screens/Index.tsx
+++ src/screens/Index.tsx
@@ -13,7 +13,7 @@ import { getDoc, doc, setDoc } from "firebase/firestore";
 
 export default function Index() {
   const navigate = useNavigate();
-  const { addUser } = useContext(AuthContext);
+  const { addUser, language, changeLanguage } = useContext(AuthContext);
 
   const signInUser = async () => {
     try {
@@ -59,12 +59,35 @@ export default function Index() {
         <div className="flex cursor-pointer items-center gap-2 rounded-xl border border-transparent bg-white px-2 font-bold text-blue-800 transition-colors hover:border-white hover:bg-transparent hover:text-white">
           <IoCarSport size={40} /> Drive Check
         </div>
-        <div className="text-lg font-bold uppercase">Maintenence tracker</div>
+        <div className="text-lg font-bold uppercase">
+          {language === "esp"
+            ? "Control de mantenimiento"
+            : "Maintenance tracker"}
+        </div>
         <div
           onClick={signInUser}
           className="flex items-center gap-2 rounded-lg border border-white bg-white p-1 px-3 text-blue-950 transition hover:cursor-pointer hover:bg-transparent hover:text-white"
         >
-          <FcGoogle size={25} /> Sign In With Google
+          <FcGoogle size={25} />{" "}
+          {language === "esp" ? "Ingresar Con Google" : "Sign In With Google"}
+        </div>
+        <div className="flex cursor-pointer overflow-hidden rounded border border-blue-950">
+          <div
+            className={`px-1 text-white hover:bg-blue-900 ${
+              language === "esp" ? "bg-blue-800 font-bold" : "bg-slate-600"
+            }`}
+            onClick={() => changeLanguage("esp")}
+          >
+            Esp
+          </div>
+          <div
+            className={`px-1 text-white hover:bg-blue-900 ${
+              language === "eng" ? "bg-blue-800 font-bold" : "bg-slate-600"
+            }`}
+            onClick={() => changeLanguage("eng")}
+          >
+            Eng
+          </div>
         </div>
       </header>
       <main className="flex flex-1 flex-col bg-slate-200 p-8 text-blue-950">
@@ -73,13 +96,15 @@ export default function Index() {
             <div className="text-2xl">Drive Check always helps your car</div>
             <ul className="flex flex-col gap-3 pt-4">
               <li className="flex items-center gap-4">
-                <LuMilestone size={35} /> Mileage Tracking
+                <LuMilestone size={35} className="text-amber-500" /> Mileage
+                Tracking
               </li>
               <li className="flex items-center gap-4">
-                <FaOilCan size={32} /> Service reminders
+                <FaOilCan size={32} className="text-amber-500" /> Service
+                reminders
               </li>
               <li className="flex items-center gap-4">
-                <MdOutlineHistory size={32} />
+                <MdOutlineHistory size={32} className="text-amber-500" />
                 Repair's history
               </li>
             </ul>
@@ -95,7 +120,7 @@ export default function Index() {
         </div>
         <div className="mt-8 flex flex-wrap justify-center gap-4">
           <div className="flex items-center gap-2 border border-slate-400 p-2">
-            <MdAttachMoney size={35} />
+            <MdAttachMoney size={35} className="text-amber-500" />
             <div>
               <div className="text-center font-bold">Control your expenses</div>
               <div>
@@ -104,14 +129,14 @@ export default function Index() {
             </div>
           </div>
           <div className="flex items-center gap-2 border border-slate-400 p-2">
-            <FaOilCan size={35} />
+            <FaOilCan size={35} className="text-amber-500" />
             <div>
               <div className="text-center font-bold">Service reminders</div>
               <div>Don’t lose sight of your maintenance and services.</div>
             </div>
           </div>
           <div className="flex items-center gap-2 border border-slate-400 p-2">
-            <LuMilestone size={35} />
+            <LuMilestone size={35} className="text-amber-500" />
             <div>
               <div className="text-center font-bold">Mileage recording</div>
               <div>Keep your mileage always tracked easily</div>
