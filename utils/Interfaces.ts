81bf171f5b6e2132dd0d3b239463ef5f61eaa46e
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-11-07 17:19:01 -0600

    Created new interfaces for new user structure

--- utils/Interfaces.ts
+++ utils/Interfaces.ts
@@ -1,12 +1,54 @@
 import { ReactNode } from "react";
 
-export interface User {
+export interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export interface GoogleUser {
   uid: string;
   email: string;
   displayName: string;
   photoURL: string;
 }
 
-export interface AuthProviderProps {
-  children: ReactNode;
+export interface User {
+  // uid: string;
+  email: string;
+  name: string;
+  profilePicture: string;
+  joinedAt: string;
+  car: Car;
+}
+
+export interface Car {
+  summary: Summary;
+  overview: OverviewEntry[];
+  upcomingReminders: Reminder[];
+  maintenanceHistory: MaintenanceRecord[];
+}
+
+interface Summary {
+  model: string;
+  year: number;
+  mileage: number;
+  lastServiceDate: string;
+}
+
+interface OverviewEntry {
+  item: string;
+  value: string;
+  date: string;
+}
+
+interface Reminder {
+  date: string;
+  description: string;
+  mileage?: number;
+}
+
+interface MaintenanceRecord {
+  date: string;
+  description: string;
+  cost: number;
+  mileage: number;
 }
