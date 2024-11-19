b1c72d01e07c18ca6c804cdcef0ba9cb2c7e1e81
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-11-19 16:18:25 -0600

    Added OverviewItem component to Overview section

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -2,46 +2,51 @@ import { FaCarBattery } from "react-icons/fa";
 import { FaOilCan } from "react-icons/fa";
 import { GiCarWheel } from "react-icons/gi";
 import { RiTempColdFill } from "react-icons/ri";
+import { useContext } from "react";
+import { AuthContext } from "./auth/AuthContext";
+import OverviewItem from "./OverviewItem";
 
 export default function Overview() {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="flex flex-col gap-4">
       <div className="font-bold text-yellow-300">Overview</div>
       <section className="grid grid-cols-2 justify-around gap-x-12 gap-y-6 rounded-xl bg-gray-900 p-4 py-6">
-        <div className="flex items-center gap-4">
-          <div className="text-blue-400">Oil level</div>
-          <div className="text-lg font-bold">90%</div>
-          <FaOilCan size={25} className="text-yellow-300" />
-          <div>23-ago-24</div>
-        </div>
+        {user?.car.overview.map((entry, index) => {
+          let icon;
+          switch (entry.name.toLowerCase()) {
+            case "oil level":
+              icon = <FaOilCan size={25} className="text-yellow-300" />;
+              break;
+            case "battery level":
+              icon = <FaCarBattery size={19} className="text-yellow-300" />;
+              break;
+            case "tire pressure":
+              icon = <GiCarWheel size={22} className="text-yellow-300" />;
+              break;
+            case "coolant level":
+              icon = <RiTempColdFill size={25} className="text-yellow-300" />;
+              break;
+            case "steering wheel oil level":
+              icon = <RiTempColdFill size={25} className="text-yellow-300" />;
+              break;
+            default:
+              icon = <RiTempColdFill size={25} className="text-yellow-300" />;
+              break;
+          }
 
-        <div className="flex items-center gap-4">
-          <div className="text-blue-400">Battery Voltage</div>
-          <div className="text-lg font-bold">12.8 v</div>
-          <FaCarBattery size={19} className="text-yellow-300" />
-          <div>23-ago-24</div>
-        </div>
-
-        <div className="flex items-center gap-4">
-          <div className="text-blue-400">Tire pressure</div>
-          <div className="text-lg font-bold">35 psi</div>
-          <GiCarWheel size={22} className="text-yellow-300" />
-          <div>23-ago-24</div>
-        </div>
-
-        <div className="flex items-center gap-4">
-          <div className="text-blue-400">Coolant Level</div>
-          <div className="text-lg font-bold">100%</div>
-          <RiTempColdFill size={25} className="text-yellow-300" />
-          <div>23-ago-24</div>
-        </div>
-
-        <div className="flex items-center gap-4">
-          <div className="text-blue-400">Steering Wheel Level</div>
-          <div className="text-lg font-bold">100%</div>
-          <RiTempColdFill size={25} className="text-yellow-300" />
-          <div>23-ago-24</div>
-        </div>
+          return (
+            <OverviewItem
+              key={index}
+              name={entry.name}
+              level={entry.level}
+              date={entry.date}
+            >
+              {icon}
+            </OverviewItem>
+          );
+        })}
       </section>
     </div>
   );
