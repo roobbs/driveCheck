9fb868a05bf2119ebc1383a3f2eedd788cfb070f
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-11-21 17:26:24 -0600

    Translated OverviewItem

--- src/components/OverviewItem.tsx
+++ src/components/OverviewItem.tsx
@@ -1,20 +1,27 @@
 import type { OverviewEntry } from "../../utils/Interfaces";
 import type { PropsWithChildren } from "react";
+import { useContext } from "react";
+import { AuthContext } from "./auth/AuthContext";
 
 export default function OverviewItem(props: PropsWithChildren<OverviewEntry>) {
-  const { name, level, date, children } = props;
+  const { language } = useContext(AuthContext);
+  const { name, nameEs, level, date, children } = props;
+
+  const data = language === "esp" ? "Sin registro" : "No Data";
+  const checked = language === "esp" ? "Sin revisar" : "No checked";
+
   return (
     <div className="flex items-center gap-4">
-      <div className="text-blue-400">{name}</div>
+      <div className="text-blue-400">{language === "esp" ? nameEs : name}</div>
       {name.includes("Battery") && (
-        <div className="text-lg font-bold">{level ? level : "No Data"}</div>
+        <div className="text-lg font-bold">{level ? level : data}</div>
       )}
       {name.includes("Tire") && (
-        <div className="text-lg font-bold">{level ? level : "No Data"}</div>
+        <div className="text-lg font-bold">{level ? level : data}</div>
       )}
 
       {children}
-      <div>{date ? date : "No checked"}</div>
+      <div>{date ? date : checked}</div>
     </div>
   );
 }
