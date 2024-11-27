c683061544ad85ea051d7a3b347780d0d6935ce7
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-11-27 15:26:15 -0600

    Added MaintenanceRecord to MaintenanceHistory

--- src/components/MaintenanceHistory.tsx
+++ src/components/MaintenanceHistory.tsx
@@ -1,38 +1,37 @@
-import { MdOutlineMedicalServices } from "react-icons/md";
-// import { FaCirclePlus } from "react-icons/fa6";
+import { FaCirclePlus } from "react-icons/fa6";
+import MaintenanceRecord from "./MaintenanceRecord";
+import { useContext } from "react";
+import { AuthContext } from "./auth/AuthContext";
 
 export default function MaintenanceHistory() {
+  const { user } = useContext(AuthContext);
+  const records = user?.car.maintenanceHistory;
+  console.log(records);
+
   return (
     <div className="flex flex-col gap-4">
       <div className="font-bold text-yellow-300">Maintenance History</div>
       <section className="relative flex flex-col flex-wrap gap-x-12 gap-y-6 rounded-xl bg-gray-900 p-4 py-6">
-        <div className="flex items-center gap-4 rounded border border-white p-2">
-          <MdOutlineMedicalServices size={25} className="text-yellow-300" />
-          <div className="text-lg font-bold">Oil Change</div>
-          <div>23-ago-24</div>
-          <div> 223,444 mi</div>
-          <div>$ 500</div>
-        </div>
-
-        <div className="flex items-center gap-4 rounded border border-white p-2">
-          <MdOutlineMedicalServices size={25} className="text-yellow-300" />
-          <div className="text-lg font-bold">Battery replacement</div>
-          <div>23-ago-24</div>
-          <div> 223,444 mi</div>
-          <div>$ 500</div>
-        </div>
-
-        <div className="flex items-center gap-4 rounded border border-white p-2">
-          <MdOutlineMedicalServices size={25} className="text-yellow-300" />
-          <div className="text-lg font-bold">Voltage regulator</div>
-          <div>23-ago-24</div>
-          <div> 223,444 mi</div>
-          <div>$ 500</div>
-        </div>
-
-        {/* <div style={{ top: 30, right: 30 }} className="absolute ">
+        <div className="">
           <FaCirclePlus size={45} className="text-yellow-300" />
-        </div> */}
+        </div>
+        {records?.length === 0 && (
+          <div className="text-center text-xl font-bold">
+            Add your first maintenance record here
+          </div>
+        )}
+        {records && records?.length > 0 && (
+          <>
+            {records?.map((rec) => {
+              <MaintenanceRecord
+                description={rec.description}
+                date={rec.date}
+                cost={rec.cost}
+                mileage={rec.mileage}
+              />;
+            })}
+          </>
+        )}
       </section>
     </div>
   );
