20d83be6ab2583532b5b66a0a7e4dca5f5a4f247
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-11-28 20:31:02 -0600

    Added AddRecordModal to MaintenanceHistory

--- src/components/MaintenanceHistory.tsx
+++ src/components/MaintenanceHistory.tsx
@@ -1,9 +1,11 @@
 import { FaCirclePlus } from "react-icons/fa6";
 import MaintenanceRecord from "./MaintenanceRecord";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "./auth/AuthContext";
+import AddRecordModal from "./AddRecordModal";
 
 export default function MaintenanceHistory() {
+  const [modalOpen, setModalOpen] = useState(false);
   const { user } = useContext(AuthContext);
   const records = user?.car.maintenanceHistory;
   console.log(records);
@@ -12,7 +14,7 @@ export default function MaintenanceHistory() {
     <div className="flex flex-col gap-4">
       <div className="font-bold text-yellow-300">Maintenance History</div>
       <section className="relative flex flex-col flex-wrap gap-x-12 gap-y-6 rounded-xl bg-gray-900 p-4 py-6">
-        <div className="">
+        <div className="" onClick={() => setModalOpen(true)}>
           <FaCirclePlus size={45} className="text-yellow-300" />
         </div>
         {records?.length === 0 && (
@@ -32,6 +34,7 @@ export default function MaintenanceHistory() {
             })}
           </>
         )}
+        {modalOpen && <AddRecordModal closeModal={() => setModalOpen(false)} />}
       </section>
     </div>
   );
