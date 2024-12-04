1b2ff4f376000a514a1b2b4c8efe01219845ac0d
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-12-03 18:47:30 -0600

    Typed Remider

--- src/components/Reminder.tsx
+++ src/components/Reminder.tsx
@@ -1,14 +1,17 @@
 import { MdNotificationsNone } from "react-icons/md";
+import type { Reminder } from "../../utils/Interfaces";
 
-export default function Remider() {
+export default function Remider(props: Reminder) {
+  const { date, description, mileage } = props;
+
   return (
     <div className="flex items-center gap-3 rounded border border-white p-2">
       <MdNotificationsNone size={25} className="text-yellow-300" />
       <div className="flex flex-col items-center gap-1">
-        <div className="text-lg font-bold">Battery replacement</div>
+        <div className="text-lg font-bold">{description}</div>
         <div>
           <div className="font-bold italic text-blue-400">
-            Due in 231,750 mi | 23-feb-24
+            Due in {mileage} | {date}
           </div>
         </div>
       </div>
