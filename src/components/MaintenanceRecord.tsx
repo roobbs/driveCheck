df06d217a9fa2ab048683ca671edc63162026d74
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-12-06 15:56:17 -0600

    Improve MaintenanceRecord style

--- src/components/MaintenanceRecord.tsx
+++ src/components/MaintenanceRecord.tsx
@@ -5,23 +5,26 @@ export default function MaintenanceRecord(props: MaintenanceRecord) {
   const { date, description, cost, mileage } = props;
 
   return (
-    <div className="flex flex-col gap-2 rounded border border-white bg-slate-900 p-4">
+    <div className="flex flex-col gap-4 rounded border border-gray-500 bg-gray-800 p-4 shadow-md">
       <div className="flex items-center gap-4">
-        <MdOutlineMedicalServices size={25} className="text-yellow-300" />
-        <div className="text-xl font-bold">{description}</div>
+        <MdOutlineMedicalServices size={35} className="text-yellow-300" />
+        <div className="text-lg font-bold text-white">{description}</div>
       </div>
-      <div className="mt-2 flex gap-6">
+
+      <div className="grid grid-cols-3 gap-6 text-center">
         <div>
           <div className="text-sm font-bold text-gray-400">Date</div>
-          <div className="font-bold">{date}</div>
+          <div className="font-semibold text-blue-400">{date}</div>
         </div>
+
         <div>
           <div className="text-sm font-bold text-gray-400">Mileage</div>
-          <div className="font-bold">{mileage} km</div>
+          <div className="font-semibold text-blue-400">{mileage} km</div>
         </div>
+
         <div>
           <div className="text-sm font-bold text-gray-400">Cost</div>
-          <div className="font-bold">${cost}</div>
+          <div className="font-semibold text-blue-400">${cost}</div>
         </div>
       </div>
     </div>
