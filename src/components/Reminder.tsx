598b4a85d5319ae4a632e9ec2819144522520b19
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-12-06 15:56:55 -0600

    Improve Reminder card style

--- src/components/Reminder.tsx
+++ src/components/Reminder.tsx
@@ -5,14 +5,19 @@ export default function Reminder(props: Reminder) {
   const { date, description, mileage } = props;
 
   return (
-    <div className="flex items-center gap-3 rounded border border-white p-2">
-      <MdNotificationsNone size={25} className="text-yellow-300" />
-      <div className="flex flex-col items-center gap-1">
-        <div className="text-lg font-bold">{description}</div>
-        <div>
-          <div className="font-bold italic text-blue-400">
-            Due in {mileage} | {date}
-          </div>
+    <div className="flex items-center gap-4 rounded border border-gray-500 bg-gray-800 p-4 shadow-md">
+      {/* Icono */}
+      <div className="flex items-center justify-center">
+        <MdNotificationsNone size={35} className="text-yellow-300" />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <div className="text-lg font-bold text-white">{description}</div>
+
+        <div className="text-md text-gray-400">
+          Due at{" "}
+          <span className="font-semibold text-blue-400">{mileage} km</span> or
+          on <span className="font-semibold text-blue-400">{date}</span>
         </div>
       </div>
     </div>
