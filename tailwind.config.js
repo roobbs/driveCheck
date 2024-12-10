6ffb84c242623bb89729fe936cba8a82a5792890
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-12-10 15:07:33 -0600

    Added more media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,17 @@ export default {
       // => @media (max-width: 767px) { ... }
 
       "1180p": { max: "1180px" },
+      "1000p": { max: "1000px" },
+      "900p": { max: "900px" },
+      "780p": { max: "780px" },
+      "750p": { max: "750px" },
+      "680p": { max: "680px" },
+      "650p": { max: "650px" },
+      "580p": { max: "580px" },
+      "490p": { max: "490px" },
+      "400p": { max: "400px" },
+      "390p": { max: "390px" },
+      "372p": { max: "372px" },
     },
   },
   plugins: [],
