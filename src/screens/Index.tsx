1bc66e7f24fee26b98fa4ab91bf0b79e104c4b73
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-12-09 19:47:21 -0600

    Improved Index style

--- src/screens/Index.tsx
+++ src/screens/Index.tsx
@@ -97,7 +97,7 @@ export default function Index() {
   };
 
   return (
-    <div className="relative">
+    <div>
       <header className="sticky top-0 flex items-center justify-between gap-4 bg-blue-950 px-8 py-3">
         <div className="1000p:flex-col-reverse flex items-center gap-2">
           <div className="flex cursor-pointer items-center gap-2 rounded-xl border border-transparent bg-white px-2 font-bold text-blue-800 transition-colors hover:border-white hover:bg-transparent hover:text-white">
@@ -138,29 +138,30 @@ export default function Index() {
           </div>
         </div>
       </header>
-      <main className="flex flex-1 flex-col bg-slate-200 p-8 text-blue-950">
+
+      <main className="flex flex-1 flex-col bg-slate-50 p-8 text-gray-800">
         <div className="flex flex-wrap items-center justify-around">
-          <div className="text-xl font-bold text-blue-950">
-            <div className="border-b-4 border-yellow-500 py-4 text-center text-2xl font-bold uppercase text-blue-950">
+          <div className="text-xl font-bold">
+            <div className="rounded-xl bg-blue-100 px-4 py-2 text-center text-2xl text-blue-600 shadow-md">
               {language === "esp"
                 ? "Drive Check te ayuda con tu auto"
                 : "Drive Check always helps your car"}
             </div>
-            <ul className="flex flex-col gap-3 pt-4">
+            <ul className="flex flex-col gap-4 pt-4">
               <li className="flex items-center gap-4">
-                <LuMilestone size={35} className="text-amber-500" />{" "}
+                <LuMilestone size={35} className="text-blue-500" />
                 {language === "esp"
                   ? "Seguimiento de kilometraje"
                   : "Mileage Tracking"}
               </li>
               <li className="flex items-center gap-4">
-                <FaOilCan size={32} className="text-amber-500" />{" "}
+                <FaOilCan size={32} className="text-blue-500" />
                 {language === "esp"
                   ? "Recordatorios de Mantenimiento"
                   : "Service reminders"}
               </li>
               <li className="flex items-center gap-4">
-                <MdOutlineHistory size={32} className="text-amber-500" />
+                <MdOutlineHistory size={32} className="text-blue-500" />
                 {language === "esp"
                   ? "Historial de reparaciones"
                   : "Repair's history"}
@@ -173,49 +174,53 @@ export default function Index() {
             className="w-1/2"
           />
         </div>
-        <div className="text-center text-xl font-bold text-blue-950">
-          {language === "esp" ? "CARACTERÍSTICAS DE LA APP" : "APP FEATURES"}
+
+        <div className="mt-12 border-b-2 border-blue-300 text-center text-2xl font-bold text-blue-600">
+          {language === "esp" ? "Características de la App" : "App Features"}
         </div>
-        <div className="mt-8 flex flex-wrap justify-center gap-4">
-          <div className="flex items-center gap-2 border border-slate-400 p-2">
-            <MdAttachMoney size={35} className="text-amber-500" />
+
+        <div className="mt-8 flex flex-wrap justify-center gap-6">
+          <div className="flex items-center gap-2 rounded-lg border border-gray-300 bg-white p-4 shadow-lg">
+            <MdAttachMoney size={35} className="text-yellow-400" />
             <div>
-              <div className="text-center font-bold">
+              <div className="text-center font-bold text-gray-800">
                 {language === "esp"
                   ? "Controla tus gastos"
                   : "Control your expenses"}
               </div>
-              <div>
+              <div className="text-sm text-gray-600">
                 {language === "esp"
                   ? "Conoce los costos de uso de tu vehículo y planea tus gastos."
                   : "Know your vehicle's running costs and plan for your expenses."}
               </div>
             </div>
           </div>
-          <div className="flex items-center gap-2 border border-slate-400 p-2">
-            <FaOilCan size={35} className="text-amber-500" />
+
+          <div className="flex items-center gap-2 rounded-lg border border-gray-300 bg-white p-4 shadow-lg">
+            <FaOilCan size={35} className="text-blue-500" />
             <div>
-              <div className="text-center font-bold">
+              <div className="text-center font-bold text-gray-800">
                 {language === "esp"
                   ? "Recordatorios de Mantenimiento"
                   : "Service reminders"}
               </div>
-              <div>
+              <div className="text-sm text-gray-600">
                 {language === "esp"
                   ? "No pierdas de vista el mantenimiento y los servicios de tu auto."
                   : "Don’t lose sight of your maintenance and services."}
               </div>
             </div>
           </div>
-          <div className="flex items-center gap-2 border border-slate-400 p-2">
-            <LuMilestone size={35} className="text-amber-500" />
+
+          <div className="flex items-center gap-2 rounded-lg border border-gray-300 bg-white p-4 shadow-lg">
+            <LuMilestone size={35} className="text-blue-500" />
             <div>
-              <div className="text-center font-bold">
+              <div className="text-center font-bold text-gray-800">
                 {language === "esp"
                   ? "Guarda tu kilometraje"
                   : "Mileage recording"}
               </div>
-              <div>
+              <div className="text-sm text-gray-600">
                 {language === "esp"
                   ? "Lleva el seguimiento de tu kilometraje de forma fácil y constante."
                   : "Keep your mileage always tracked easily."}
