82c852920d4191056adefcdc451579fd4ac2d2e5
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-12-10 19:00:16 -0600

    Change upcomingReminders style to make it responsive

--- src/components/UpcomingReminders.tsx
+++ src/components/UpcomingReminders.tsx
@@ -12,33 +12,34 @@ export default function UpcomingReminders() {
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="font-bold text-yellow-300">Upcoming Reminders</div>
-      <section className="flex flex-wrap items-center gap-x-12 gap-y-6 rounded-xl bg-gray-900 p-4 py-6">
+      <div className="flex items-center gap-12">
+        <div className="font-bold text-yellow-300">Upcoming Reminders</div>
+        <div onClick={() => setModalOpen(true)}>
+          <FaCirclePlus
+            size={45}
+            className="text-yellow-300 transition hover:text-yellow-400"
+          />
+        </div>
+      </div>
+      <section className="grid grid-cols-2 justify-around gap-6 rounded-xl bg-gray-900 p-4 py-6 680p:grid-cols-1">
         {reminders?.length === 0 && (
           <div className="text-center text-xl font-bold">
             Add your first reminder here
           </div>
         )}
-        <div className="flex items-center gap-6">
-          {reminders && reminders.length > 0 && (
-            <>
-              {reminders?.map((rec, index) => (
-                <Reminder
-                  key={index}
-                  description={rec.description}
-                  date={rec.date}
-                  mileage={rec.mileage}
-                />
-              ))}
-            </>
-          )}
-          <div onClick={() => setModalOpen(true)}>
-            <FaCirclePlus
-              size={45}
-              className="text-yellow-300 transition hover:text-yellow-400"
-            />
-          </div>
-        </div>
+        {reminders && reminders.length > 0 && (
+          <>
+            {reminders?.map((rec, index) => (
+              <Reminder
+                key={index}
+                description={rec.description}
+                date={rec.date}
+                mileage={rec.mileage}
+              />
+            ))}
+          </>
+        )}
+
         {modalOpen && (
           <AddReminderModal closeModal={() => setModalOpen(false)} />
         )}
