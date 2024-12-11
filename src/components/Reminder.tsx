029f9aacf0788b045f03cc7e81f94afba4624fe0
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-12-11 15:05:21 -0600

    Reminder card is responsive now

--- src/components/Reminder.tsx
+++ src/components/Reminder.tsx
@@ -5,18 +5,22 @@ export default function Reminder(props: Reminder) {
   const { date, description, mileage } = props;
 
   return (
-    <div className="flex items-center gap-4 rounded border border-gray-500 bg-gray-800 p-4 shadow-md">
-      <div className="flex items-center justify-center">
-        <MdNotificationsNone size={35} className="text-yellow-300" />
+    <div className="flex flex-col items-center justify-between gap-4 rounded-lg border border-gray-500 bg-gray-800 p-4 shadow-md">
+      <div className="flex w-full items-center justify-between gap-4">
+        <div className="flex justify-center">
+          <MdNotificationsNone size={30} className="text-yellow-300" />
+        </div>
+        <div className="text-lg font-bold text-blue-100">{description}</div>
+        <div> </div>
       </div>
 
-      <div className="flex flex-col gap-2">
-        <div className="text-lg font-bold text-blue-100">{description}</div>
-
-        <div className="text-md text-gray-400">
+      <div className="text-md flex flex-wrap justify-center gap-2 tracking-wider text-gray-400">
+        <div>
           Due at{" "}
-          <span className="font-semibold text-blue-400">{mileage} km</span> or
-          on <span className="font-semibold text-blue-400">{date}</span>
+          <span className="font-semibold text-blue-400">{mileage} km</span>{" "}
+        </div>
+        <div>
+          or on <span className="font-semibold text-blue-400">{date}</span>
         </div>
       </div>
     </div>
