58bfd22d1fcf41e282b3dbc5f713ec7a20b79635
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-12-14 14:39:56 -0600

    Use new animation for manu in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ import { signUserOut } from "../../utils/database";
 import { useNavigate } from "react-router-dom";
 import { MdLogout } from "react-icons/md";
 import { IoClose } from "react-icons/io5";
+import "../styles/animations.css";
 
 function Header() {
   const [menu, setMenu] = useState(false);
@@ -44,48 +45,48 @@ function Header() {
           />
         )}
       </div>
-      {menu && (
-        <div className="absolute right-4 top-full z-10 flex w-1/3 flex-col gap-4 bg-white p-2 text-blue-800 900p:w-1/2 650p:right-0 650p:w-full">
-          <div className="flex items-center gap-4">
-            <img
-              src={user?.profilePicture}
-              alt="user"
-              className="h-11 rounded-full"
-            />
-            <div className="text-lg">{user?.name}</div>
+      <div
+        className={`absolute right-4 top-full z-10 w-1/3 flex-col gap-4 bg-white p-2 text-blue-800 transition 900p:w-1/2 650p:right-0 650p:w-full ${menu ? "expand" : "vanish hidden"}`}
+      >
+        <div className="flex items-center gap-4">
+          <img
+            src={user?.profilePicture}
+            alt="user"
+            className="h-11 rounded-full"
+          />
+          <div className="text-lg">{user?.name}</div>
+        </div>
+        <div className="flex items-center justify-center gap-8">
+          <div className="text-md font-bold">
+            {language === "esp" ? "Idioma" : "Language"}
           </div>
-          <div className="flex items-center justify-center gap-8">
-            <div className="text-md font-bold">
-              {language === "esp" ? "Idioma" : "Language"}
+          <div className="flex cursor-pointer rounded border border-blue-950">
+            <div
+              className={`flex items-center gap-1 px-1 text-white hover:bg-blue-900 ${
+                language === "esp" ? "bg-blue-800 font-bold" : "bg-slate-600"
+              }`}
+              onClick={() => changeLanguage("esp")}
+            >
+              Es <img src="../../assets/es_MX.png" alt="" />
             </div>
-            <div className="flex cursor-pointer rounded border border-blue-950">
-              <div
-                className={`flex items-center gap-1 px-1 text-white hover:bg-blue-900 ${
-                  language === "esp" ? "bg-blue-800 font-bold" : "bg-slate-600"
-                }`}
-                onClick={() => changeLanguage("esp")}
-              >
-                Es <img src="../../assets/es_MX.png" alt="" />
-              </div>
-              <div
-                className={`flex items-center gap-1 px-1 text-white hover:bg-blue-900 ${
-                  language === "eng" ? "bg-blue-800 font-bold" : "bg-slate-600"
-                }`}
-                onClick={() => changeLanguage("eng")}
-              >
-                En <img src="../../assets/en_US.png" alt="" />
-              </div>
+            <div
+              className={`flex items-center gap-1 px-1 text-white hover:bg-blue-900 ${
+                language === "eng" ? "bg-blue-800 font-bold" : "bg-slate-600"
+              }`}
+              onClick={() => changeLanguage("eng")}
+            >
+              En <img src="../../assets/en_US.png" alt="" />
             </div>
-          </div>
-          <div
-            onClick={logOut}
-            className="flex cursor-pointer items-center justify-center gap-2 rounded-sm bg-blue-900 text-center font-bold text-white transition hover:bg-blue-800 650p:w-1/2 650p:self-center"
-          >
-            <MdLogout size={25} />{" "}
-            {language === "esp" ? "Cerrar Sesión" : "Log Out"}
           </div>
         </div>
-      )}
+        <div
+          onClick={logOut}
+          className="flex cursor-pointer items-center justify-center gap-2 rounded-sm bg-blue-900 text-center font-bold text-white transition hover:bg-blue-800 650p:w-1/2 650p:self-center"
+        >
+          <MdLogout size={25} />{" "}
+          {language === "esp" ? "Cerrar Sesión" : "Log Out"}
+        </div>
+      </div>
     </header>
   );
 }
