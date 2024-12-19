151b3294aec56ada9c780c7dcc8df2f8bccfd007
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-12-19 14:34:43 -0600

    Modified Home screen and deleted Content

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,15 @@
-import Header from "../components/Header";
-import Content from "../components/Content";
+import Summary from "../components/Summary";
+import Overview from "../components/Overview";
+import UpcomingReminders from "../components/UpcomingReminders";
+import MaintenanceHistory from "../components/MaintenanceHistory";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <Content />
-    </>
+    <main className="flex flex-1 flex-col gap-8 bg-gray-800 p-4">
+      <Summary />
+      <Overview />
+      <UpcomingReminders />
+      <MaintenanceHistory />
+    </main>
   );
 }
