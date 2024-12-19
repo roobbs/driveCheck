236682a06a1d6cbc57038409ca82748380d51eb9
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-12-19 15:31:07 -0600

    Created NavMenu to navigate between pages

---
+++ src/components/NavMenu.tsx
@@ -0,0 +1,47 @@
+import { NavLink } from "react-router-dom";
+import { MdHomeFilled } from "react-icons/md";
+import { IoMdNotifications } from "react-icons/io";
+import { RiToolsFill } from "react-icons/ri";
+
+export default function NavMenu() {
+  return (
+    <nav className="sticky top-0 flex items-center justify-around bg-slate-950 px-4 py-2 shadow-md">
+      <NavLink
+        to="/home"
+        className={({ isActive }) =>
+          `flex items-center gap-1 rounded px-4 py-1 text-sm font-medium transition duration-300 ${
+            isActive
+              ? "text-blue-500"
+              : "text-slate-300 hover:bg-slate-700 hover:text-white"
+          }`
+        }
+      >
+        <MdHomeFilled size={18} /> Home
+      </NavLink>
+      <NavLink
+        to="/reminders"
+        className={({ isActive }) =>
+          `flex items-center gap-1 rounded px-4 py-1 text-sm font-medium transition duration-300 ${
+            isActive
+              ? "text-blue-500"
+              : "text-slate-300 hover:bg-slate-700 hover:text-white"
+          } `
+        }
+      >
+        <IoMdNotifications size={20} /> Reminders
+      </NavLink>
+      <NavLink
+        to="/maintenance"
+        className={({ isActive }) =>
+          `flex items-center gap-1 rounded px-4 py-1 text-sm font-medium transition duration-300 ${
+            isActive
+              ? "text-blue-500"
+              : "text-slate-300 hover:bg-slate-700 hover:text-white"
+          } `
+        }
+      >
+        <RiToolsFill size={21} /> Maintenance History
+      </NavLink>
+    </nav>
+  );
+}
