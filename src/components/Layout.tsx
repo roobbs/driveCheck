da4188a0af2ef8988877f248226423ad27f48e18
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-12-19 15:31:40 -0600

    Added NavMenu to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import NavMenu from "./NavMenu";
 
 export default function Layout() {
   return (
     <>
       <Header />
+      <NavMenu />
       <Outlet />
     </>
   );
