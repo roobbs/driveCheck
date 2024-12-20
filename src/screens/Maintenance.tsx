6b2fad3a701b96bc3044ae9f05792933230260ac
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-12-20 11:23:22 -0600

    Maintenance screens si now responsive

--- src/screens/Maintenance.tsx
+++ src/screens/Maintenance.tsx
@@ -7,15 +7,17 @@ export default function Maintenance() {
   const { user, language } = useContext(AuthContext);
   const maintenanceHistory = (user?.car?.maintenanceHistory || []).sort(
     (a, b) => {
-      const dateA = new Date(a.date);
-      const dateB = new Date(b.date);
+      const dateA = new Date(a.date).getTime();
+      const dateB = new Date(b.date).getTime();
       return dateB - dateA;
     },
   );
 
   return (
-    <main className="flex flex-1 flex-col gap-8 overflow-scroll bg-gray-800 p-4">
-      <h1 className="text-xl font-bold text-yellow-300">Maintenance History</h1>
+    <main className="flex flex-1 flex-col gap-8 overflow-x-scroll bg-gray-800 p-4">
+      <h1 className="sticky left-1 text-xl font-bold text-yellow-300">
+        Maintenance History
+      </h1>
       <table className="w-full border-collapse overflow-hidden rounded-lg shadow-lg shadow-gray-950">
         <thead>
           <tr className="bg-gray-900 text-yellow-300">
@@ -47,16 +49,16 @@ export default function Maintenance() {
                 index % 2 === 0 ? "bg-slate-700" : "bg-slate-800"
               }`}
             >
-              <td className="p-3 font-medium text-gray-300">
+              <td className="whitespace-nowrap p-3 font-medium text-gray-300">
                 {formatDate(record.date, language)}
               </td>
               <td className="p-3 font-medium text-blue-100">
                 {record.description}
               </td>
-              <td className="p-3 font-medium text-green-500">
+              <td className="whitespace-nowrap p-3 font-medium text-green-500">
                 ${formatNumber(record.cost)}
               </td>
-              <td className="p-3 font-medium text-blue-400">
+              <td className="whitespace-nowrap p-3 font-medium text-blue-400">
                 {`${formatNumber(record.odometer)} ${user?.unitOfMeasure}`}
               </td>
             </tr>
