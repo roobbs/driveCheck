7bb5855ad91d7ce8cec9a5e9a6f8a8d4d84a537b
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-12-25 15:51:39 -0600

    Improved Reminders screen

--- src/screens/Reminders.tsx
+++ src/screens/Reminders.tsx
@@ -7,29 +7,31 @@ export default function Reminders() {
   const reminders = user?.car?.upcomingReminders || [];
 
   return (
-    <main className="flex flex-1 flex-col gap-6 bg-gray-800 p-4">
-      <h1 className="text-2xl font-bold text-yellow-300">
+    <main className="flex flex-1 flex-col gap-6 bg-gray-800 p-6">
+      <h1 className="text-xl font-bold text-yellow-300">
         {language === "esp" ? "Recordatorios" : "Reminders"}
       </h1>
 
-      {reminders.length === 0 ? (
-        <div className="text-center text-lg text-gray-300">
-          {language === "esp"
-            ? "No tienes recordatorios programados."
-            : "You have no scheduled reminders."}
-        </div>
-      ) : (
-        <div className="sm:grid-cols-2 grid grid-cols-1 gap-4 lg:grid-cols-3">
-          {reminders.map((reminder, index) => (
-            <Reminder
-              key={index}
-              date={reminder.date}
-              description={reminder.description}
-              odometer={reminder.odometer}
-            />
-          ))}
-        </div>
-      )}
+      <div className="flex flex-col gap-4 rounded-lg bg-gray-900 p-6 shadow-lg">
+        {reminders.length === 0 ? (
+          <div className="text-center text-lg text-gray-300">
+            {language === "esp"
+              ? "No tienes recordatorios programados."
+              : "You have no scheduled reminders."}
+          </div>
+        ) : (
+          <div className="sm:grid-cols-2 grid grid-cols-1 gap-4 lg:grid-cols-3">
+            {reminders.map((reminder, index) => (
+              <Reminder
+                key={index}
+                date={reminder.date}
+                description={reminder.description}
+                odometer={reminder.odometer}
+              />
+            ))}
+          </div>
+        )}
+      </div>
     </main>
   );
 }
