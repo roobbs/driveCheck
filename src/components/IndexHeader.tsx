1579a0610118ed811cdb8935a687647c62374e1e
Carlos S. <carlossalmeroncabrera@gmail.com>
2025-01-01 12:46:02 -0600

    Added SignInButon to IndexHeader

--- src/components/IndexHeader.tsx
+++ src/components/IndexHeader.tsx
@@ -1,14 +1,12 @@
 import enImage from "../images/en_US.png";
 import esImage from "../images/es_MX.png";
 import { IoCarSport } from "react-icons/io5";
-import { FcGoogle } from "react-icons/fc";
 import { useContext } from "react";
 import { AuthContext } from "./auth/AuthContext";
-import { useSignIn } from "../hooks/useSignIn";
+import SignInButton from "./auth/SignInButton";
 
 export default function IndexHeader() {
   const { language, changeLanguage } = useContext(AuthContext);
-  const { signInUser } = useSignIn();
 
   return (
     <header className="sticky top-0 flex items-center justify-between gap-4 bg-blue-950 px-8 py-3">
@@ -24,13 +22,7 @@ export default function IndexHeader() {
         </div>
       </div>
       <div className="flex items-center gap-8 750p:gap-4 680p:flex-col-reverse 680p:gap-3">
-        <div
-          onClick={signInUser}
-          className="flex items-center gap-2 rounded-lg border border-white bg-white p-1 px-3 text-blue-950 transition hover:cursor-pointer hover:bg-transparent hover:text-white"
-        >
-          <FcGoogle size={25} />{" "}
-          {language === "esp" ? "Ingresar Con Google" : "Sign In With Google"}
-        </div>
+        <SignInButton />
         <div className="flex cursor-pointer overflow-hidden rounded border border-blue-950">
           <div
             className={`flex items-center gap-1 px-1 text-white hover:bg-blue-900 ${
