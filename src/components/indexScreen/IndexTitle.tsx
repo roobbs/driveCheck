6bd4a613065e767ed36d540860a4308925bd8a21
Carlos S. <carlossalmeroncabrera@gmail.com>
2025-01-03 13:08:56 -0600

    Fix IndexTitle style error

--- src/components/indexScreen/IndexTitle.tsx
+++ src/components/indexScreen/IndexTitle.tsx
@@ -4,9 +4,11 @@ interface Title {
 
 export default function IndexTitle({ title }: Title) {
   return (
-    <h2 className="via--900 relative mb-6 bg-gradient-to-r from-sky-950 via-blue-600 to-sky-950 bg-clip-text text-center text-4xl font-bold tracking-tight text-transparent">
-      {title}
-      <span className="absolute bottom-[-10px] left-1/2 h-1 w-1/3 -translate-x-1/2 rounded-full bg-gradient-to-r from-blue-900 to-blue-500"></span>
-    </h2>
+    <div className="mb-4 flex flex-col items-center gap-1">
+      <h2 className="bg-gradient-to-r from-sky-950 via-blue-600 to-sky-950 bg-clip-text text-center text-4xl font-bold text-transparent">
+        {title}
+      </h2>
+      <div className="h-1 w-1/3 rounded-full bg-gradient-to-r from-blue-900 to-blue-500"></div>
+    </div>
   );
 }
