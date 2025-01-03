0de95122a3935a00860606b5624998f7511bab82
Carlos S. <carlossalmeroncabrera@gmail.com>
2025-01-03 13:37:39 -0600

    Added button for github on Index screen

--- src/screens/Index.tsx
+++ src/screens/Index.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../components/auth/AuthContext";
+import { FaGithub } from "react-icons/fa";
 
 import IndexHeader from "../components/IndexHeader";
 import HeroSection from "../components/indexScreen/HeroSection";
@@ -16,9 +17,19 @@ export default function Index() {
         <HeroSection />
         <BenefitsSection />
         <FeaturesSection />
-        <div className="text-right text-blue-900">
-          {language === "esp" ? "Creado por:" : "Created by:"}{" "}
-          <span className="text-lg font-bold text-gray-800">roobbs</span>
+        <div className="mt-8 flex justify-end">
+          <a
+            href="https://github.com/roobbs"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group flex items-center gap-2 rounded-lg bg-gradient-to-r from-indigo-800 to-blue-600 px-4 py-2 text-white shadow-lg transition-all duration-300 hover:from-blue-600 hover:to-indigo-800 hover:shadow-xl"
+          >
+            <FaGithub className="h-6 w-6 transition-transform duration-300 group-hover:scale-110" />
+            <span className="text-lg">
+              {language === "esp" ? "Creado por " : "Created by "}
+            </span>
+            <span className="text-lg font-bold">roobbs</span>
+          </a>
         </div>
       </main>
     </>
