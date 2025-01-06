26aab93d72d78933a0eac74b74452f577efebce7
Carlos S. <carlossalmeroncabrera@gmail.com>
2025-01-06 12:31:02 -0600

    Added new icons for Reminder

--- src/components/Reminder.tsx
+++ src/components/Reminder.tsx
@@ -5,6 +5,8 @@ import { useContext } from "react";
 import formatDate from "../../utils/formatDate";
 import formatNumber from "../../utils/formatNumber";
 import getDays from "../../utils/getDays";
+import { MdOutlineDelete } from "react-icons/md";
+import { MdOutlineEdit } from "react-icons/md";
 
 export default function Reminder(props: Reminder) {
   const { date, description, odometer } = props;
@@ -13,10 +15,10 @@ export default function Reminder(props: Reminder) {
   const daysUntilReminder = getDays(date);
 
   let borderColor = "border-gray-500";
-  let textColor = "text-gray-400";
+  let textColor = "text-blue-400";
 
   if (daysUntilReminder <= 7) {
-    borderColor = "border-red-500";
+    borderColor = "border-red-700";
     textColor = "text-red-400";
   } else if (daysUntilReminder <= 14) {
     borderColor = "border-yellow-500";
@@ -25,14 +27,16 @@ export default function Reminder(props: Reminder) {
 
   return (
     <div
-      className={`flex flex-col items-center justify-between gap-2 rounded-lg border ${borderColor} bg-gray-800 p-4 shadow-md 900p:p-2 680p:gap-1`}
+      className={`flex flex-col justify-between gap-2 rounded-lg border ${borderColor} bg-gray-800 p-4 shadow-md 900p:p-2 680p:gap-1`}
     >
       <div className="flex w-full items-center justify-between gap-4">
-        <div className="flex justify-center">
-          <MdNotificationsNone size={30} className="text-yellow-300" />
-        </div>
+        <MdNotificationsNone size={25} className="text-yellow-300" />
         <div className={`text-lg font-bold ${textColor}`}>{description}</div>
-        <div> </div>
+        <MdOutlineEdit
+          size={22}
+          className="text-yellow-300 transition hover:scale-125"
+          onClick={() => {}}
+        />
       </div>
 
       <div className="text-md flex w-full flex-wrap justify-around gap-1 tracking-wider text-gray-400">
@@ -48,6 +52,13 @@ export default function Reminder(props: Reminder) {
             {formatNumber(odometer ? odometer : 0)} {user?.unitOfMeasure}
           </span>{" "}
         </div>
+      </div>
+
+      <div
+        className="flex justify-center text-red-500 transition duration-300 hover:scale-110 hover:text-red-600"
+        onClick={() => {}}
+      >
+        <MdOutlineDelete size={22} />
       </div>
     </div>
   );
