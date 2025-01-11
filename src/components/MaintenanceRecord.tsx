90c0e75a4d1c690b598140a4fc8006b217db8080
Carlos S. <carlossalmeroncabrera@gmail.com>
2025-01-11 11:43:15 -0600

    Added button to edit record on MaintenanceRecord

--- src/components/MaintenanceRecord.tsx
+++ src/components/MaintenanceRecord.tsx
@@ -5,15 +5,18 @@ import { useContext, useState } from "react";
 import formatDate from "../../utils/formatDate";
 import formatNumber from "../../utils/formatNumber";
 import { MdOutlineDelete } from "react-icons/md";
+import { MdModeEditOutline } from "react-icons/md";
 import DeleteMaintenanceModal from "./DeleteMaintenance";
+import EditRecordModal from "./EditRecord";
 
 export default function MaintenanceRecord(props: MaintenanceRecord) {
   const { date, description, laborCost, partCost, odometer } = props;
   const { language, user } = useContext(AuthContext);
-  const [showConfirm, setShowConfirm] = useState(false);
+  const [showDelete, setShowDelete] = useState(false);
+  const [showEdit, setShowEdit] = useState(false);
 
   return (
-    <div className="flex justify-center gap-2">
+    <div className="flex justify-center gap-1">
       <div className="maintenanceContainer grid w-[900px] grid-cols-2 items-center gap-6 justify-self-center rounded border border-gray-500 bg-gray-800 p-4 shadow-md 1280p:w-[70%] 1180p:w-[80%] 900p:w-[100%] 650p:grid-cols-1 650p:gap-1">
         <div className="flex items-center gap-4">
           <MdOutlineMedicalServices size={35} className="text-yellow-300" />
@@ -55,20 +58,41 @@ export default function MaintenanceRecord(props: MaintenanceRecord) {
           </div>
         </div>
       </div>
-      <div
-        className="flex items-center justify-center text-red-500 transition duration-300 hover:scale-110 hover:text-red-600"
-        onClick={() => setShowConfirm(true)}
-      >
-        <MdOutlineDelete size={22} />
+      <div className="flex flex-col justify-center gap-5">
+        <div
+          className="flex items-center justify-center text-yellow-300 transition duration-300 hover:scale-110 hover:text-yellow-500"
+          onClick={() => {
+            setShowEdit(true);
+          }}
+        >
+          <MdModeEditOutline size={22} />
+        </div>
+        <div
+          className="flex items-center justify-center text-red-500 transition duration-300 hover:scale-110 hover:text-red-600"
+          onClick={() => setShowDelete(true)}
+        >
+          <MdOutlineDelete size={22} />
+        </div>
       </div>
-      {showConfirm && (
+
+      {showEdit && (
+        <EditRecordModal
+          date={date}
+          description={description}
+          partCost={partCost}
+          laborCost={laborCost}
+          odometer={odometer}
+          closeModal={() => setShowEdit(false)}
+        />
+      )}
+      {showDelete && (
         <DeleteMaintenanceModal
           date={date}
           description={description}
           partCost={partCost}
           laborCost={laborCost}
           odometer={odometer}
-          setShowConfirm={setShowConfirm}
+          setShowConfirm={setShowDelete}
         />
       )}
     </div>
