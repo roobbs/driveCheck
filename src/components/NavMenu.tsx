4a29a1991faab389163e81983b6bb7630fa1b9e5
Carlos S. <carlossalmeroncabrera@gmail.com>
2025-01-16 22:15:02 -0600

    Translated NavMenu

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -2,8 +2,11 @@ import { NavLink } from "react-router-dom";
 import { MdHomeFilled } from "react-icons/md";
 import { IoMdNotifications } from "react-icons/io";
 import { RiToolsFill } from "react-icons/ri";
+import { AuthContext } from "./auth/AuthContext";
+import { useContext } from "react";
 
 export default function NavMenu() {
+  const { language } = useContext(AuthContext);
   return (
     <nav className="sticky top-0 flex items-center justify-around bg-slate-950 px-4 py-2 shadow-md">
       <NavLink
@@ -16,7 +19,10 @@ export default function NavMenu() {
           }`
         }
       >
-        <MdHomeFilled size={18} /> <span className="750p:hidden">Home</span>
+        <MdHomeFilled size={18} />{" "}
+        <span className="750p:hidden">
+          {language === "esp" ? "Inicio" : "Home"}
+        </span>
       </NavLink>
       <NavLink
         to="/reminders"
@@ -29,7 +35,9 @@ export default function NavMenu() {
         }
       >
         <IoMdNotifications size={20} />{" "}
-        <span className="750p:hidden">Reminders</span>
+        <span className="750p:hidden">
+          {language === "esp" ? "Recordatorios" : "Reminders"}
+        </span>
       </NavLink>
       <NavLink
         to="/maintenance"
@@ -42,7 +50,11 @@ export default function NavMenu() {
         }
       >
         <RiToolsFill size={21} />{" "}
-        <span className="750p:hidden">Maintenance History</span>
+        <span className="750p:hidden">
+          {language === "esp"
+            ? "Historial de Mantenimiento"
+            : "Maintenance History"}
+        </span>
       </NavLink>
     </nav>
   );
