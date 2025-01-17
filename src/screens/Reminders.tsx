1d73882515cb2fa5d5b6cb7ed8ec46de72b83a71
Carlos S. <carlossalmeroncabrera@gmail.com>
2025-01-17 14:21:01 -0600

    Modified Reminders screen

--- src/screens/Reminders.tsx
+++ src/screens/Reminders.tsx
@@ -1,27 +1,56 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../components/auth/AuthContext";
 import Reminder from "../components/Reminder";
+import { FaCirclePlus } from "react-icons/fa6";
+import AddReminderModal from "../components/AddReminderModal";
 
 export default function Reminders() {
   const { user, language } = useContext(AuthContext);
+  const [modalOpen, setModalOpen] = useState(false);
+
   const reminders = user?.car?.upcomingReminders || [];
+  const currentDate = new Date();
+
+  const pendingReminders = reminders.filter((reminder) => {
+    const reminderDate = new Date(reminder.date);
+    return (
+      reminderDate.getFullYear() < currentDate.getFullYear() ||
+      (reminderDate.getFullYear() === currentDate.getFullYear() &&
+        reminderDate.getMonth() <= currentDate.getMonth())
+    );
+  });
+
+  const futureReminders = reminders.filter(
+    (reminder) => !pendingReminders.includes(reminder),
+  );
 
   return (
     <main className="flex flex-1 flex-col gap-6 bg-gray-800 p-6">
-      <h1 className="text-xl font-bold text-yellow-300">
-        {language === "esp" ? "Recordatorios" : "Reminders"}
-      </h1>
+      <div className="flex items-center justify-between">
+        <h2 className="text-xl font-bold text-yellow-300">
+          {language === "esp" ? "Recordatorios" : "Reminders"}
+        </h2>
+        <div onClick={() => setModalOpen(true)}>
+          <FaCirclePlus
+            size={45}
+            className="text-yellow-300 transition hover:text-yellow-400"
+          />
+        </div>
+      </div>
 
-      <div className="flex flex-col gap-4 rounded-lg bg-gray-900 p-6 shadow-lg">
-        {reminders.length === 0 ? (
-          <div className="text-center text-lg text-gray-300">
+      <section className="flex flex-col gap-4 rounded-lg bg-gray-900 p-6 shadow-lg">
+        <h3 className="text-center text-lg font-bold text-yellow-300">
+          {language === "esp" ? "Este mes" : "This month"}
+        </h3>
+        {pendingReminders.length === 0 ? (
+          <div className="text-center text-gray-300">
             {language === "esp"
-              ? "No tienes recordatorios programados."
-              : "You have no scheduled reminders."}
+              ? "No tienes recordatorios pendientes "
+              : "You have no pending reminders "}
           </div>
         ) : (
-          <div className="grid grid-cols-2 gap-4 780p:grid-cols-1">
-            {reminders.map((reminder, index) => (
+          <div className="grid gap-4 780p:grid-cols-1">
+            {pendingReminders.map((reminder, index) => (
               <Reminder
                 key={index}
                 date={reminder.date}
@@ -31,7 +60,37 @@ export default function Reminders() {
             ))}
           </div>
         )}
-      </div>
+      </section>
+
+      {futureReminders.length > 0 && (
+        <section className="flex flex-col gap-4 rounded-lg bg-gray-900 p-6 shadow-lg">
+          <h2 className="text-lg font-bold text-yellow-300">
+            {language === "esp"
+              ? "Próximos recordatorios"
+              : "Upcoming Reminders"}
+          </h2>
+          {futureReminders.length === 0 ? (
+            <div className="text-center text-gray-300">
+              {language === "esp"
+                ? "No tienes próximos recordatorios programados."
+                : "You have no upcoming reminders."}
+            </div>
+          ) : (
+            <div className="grid gap-4 780p:grid-cols-1">
+              {futureReminders.map((reminder, index) => (
+                <Reminder
+                  key={index}
+                  date={reminder.date}
+                  description={reminder.description}
+                  odometer={reminder.odometer}
+                />
+              ))}
+            </div>
+          )}
+        </section>
+      )}
+
+      {modalOpen && <AddReminderModal closeModal={() => setModalOpen(false)} />}
     </main>
   );
 }
