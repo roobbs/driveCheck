9350861727f878b8a9a7569f34858153cd89c2e5
Carlos S. <carlossalmeroncabrera@gmail.com>
2025-02-05 16:09:38 -0600

    Created structure folder

--- src/components/structure/PrivateRoute.tsx
+++ src/components/structure/PrivateRoute.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Outlet } from "react-router-dom";
-import { AuthContext } from "./auth/AuthContext";
-import AuthError from "../screens/AuthError";
+import { AuthContext } from "../auth/AuthContext";
+import AuthError from "../../screens/AuthError";
 
 const PrivateRoute = () => {
   const { user } = useContext(AuthContext);
