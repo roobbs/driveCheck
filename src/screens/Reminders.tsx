3b508936f276d6bf572ec269ce6b387d45629b31
Carlos S. <carlossalmeroncabrera@gmail.com>
2025-02-10 17:59:52 -0600

    Use new reminders hook in Reminders screen

--- src/screens/Reminders.tsx
+++ src/screens/Reminders.tsx
@@ -3,34 +3,23 @@ import { AuthContext } from "../components/auth/AuthContext";
 import Reminder from "../components/elements/Reminder";
 import { FaCirclePlus } from "react-icons/fa6";
 import AddReminderModal from "../components/modals/AddReminderModal";
+import useReminders from "../hooks/useReminders";
 
 export default function Reminders() {
-  const { user, language } = useContext(AuthContext);
+  const { language } = useContext(AuthContext);
   const [modalOpen, setModalOpen] = useState(false);
 
-  const reminders = user?.car?.upcomingReminders || [];
-  const currentDate = new Date();
-
-  const pendingReminders = reminders
-    .filter((reminder) => {
-      const reminderDate = new Date(reminder.date);
-      return (
-        reminderDate.getFullYear() < currentDate.getFullYear() ||
-        (reminderDate.getFullYear() === currentDate.getFullYear() &&
-          reminderDate.getMonth() <= currentDate.getMonth())
-      );
-    })
-    .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
-
-  const futureReminders = reminders
-    .filter((reminder) => !pendingReminders.includes(reminder))
-    .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+  const { urgentReminders, futureReminders } = useReminders();
 
   return (
     <main className="flex flex-1 flex-col gap-6 bg-gray-800 p-4">
       <div className="flex items-center justify-between">
         <h2 className="text-xl font-bold text-yellow-300">
           {language === "esp" ? "Recordatorios" : "Reminders"}
+          <span className="text-md text-gray-400">
+            {" "}
+            {`(${urgentReminders.length + futureReminders.length})`}
+          </span>
         </h2>
         <div onClick={() => setModalOpen(true)}>
           <FaCirclePlus
@@ -40,7 +29,7 @@ export default function Reminders() {
         </div>
       </div>
 
-      {pendingReminders.length === 0 && futureReminders.length === 0 && (
+      {urgentReminders.length === 0 && futureReminders.length === 0 && (
         <section className="flex flex-col gap-4 rounded-lg bg-gray-900 p-4 shadow-lg">
           <div className="text-center font-bold text-yellow-200">
             {language === "esp"
@@ -50,14 +39,18 @@ export default function Reminders() {
         </section>
       )}
 
-      {pendingReminders.length > 0 && (
+      {urgentReminders.length > 0 && (
         <section className="flex flex-col gap-4 rounded-lg bg-gray-900 p-4 shadow-lg">
           <h3 className="text-center text-lg font-bold text-yellow-300">
-            {language === "esp" ? "Este mes" : "This month"}
+            {language === "esp" ? "Proximos 30 dias" : "Next 30 days"}{" "}
+            <span className="text-md text-gray-400">
+              {" "}
+              {`(${urgentReminders.length})`}
+            </span>
           </h3>
 
           <div className="900p-min:justify-center grid gap-4 780p:grid-cols-1">
-            {pendingReminders.map((reminder, index) => (
+            {urgentReminders.map((reminder, index) => (
               <Reminder
                 key={index}
                 date={reminder.date}
@@ -71,11 +64,13 @@ export default function Reminders() {
 
       {futureReminders.length > 0 && (
         <section className="flex flex-col gap-4 rounded-lg bg-gray-900 p-4 shadow-lg">
-          <h2 className="text-lg font-bold text-yellow-300">
-            {language === "esp"
-              ? "Próximos recordatorios"
-              : "Upcoming Reminders"}
-          </h2>
+          <h3 className="text-lg font-bold text-yellow-300">
+            {language === "esp" ? "Próximos pendientes" : "Upcoming Reminders"}
+            <span className="text-md text-gray-400">
+              {" "}
+              {`(${futureReminders.length})`}
+            </span>
+          </h3>
 
           <div className="900p-min:justify-center grid gap-4 780p:grid-cols-1">
             {futureReminders.map((reminder, index) => (
