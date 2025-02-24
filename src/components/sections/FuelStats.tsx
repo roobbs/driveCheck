aa9c4c8c45f81158b512e573029618cd0851c9c3
Carlos S. <carlossalmeroncabrera@gmail.com>
2025-02-24 11:21:00 -0600

    Modified FuelStats

--- src/components/sections/FuelStats.tsx
+++ src/components/sections/FuelStats.tsx
@@ -1,20 +1,30 @@
 import { useContext } from "react";
 import { useFuelStats } from "../../hooks/useFuelStats";
 import { AuthContext } from "../auth/AuthContext";
+import { useMaintenanceInfo } from "../../hooks/useMaintenanceInfo";
 
 export default function FuelStats() {
   const fuelStats = useFuelStats();
   const { user, language } = useContext(AuthContext);
 
+  const { fuelCostThisYear } = useMaintenanceInfo();
+
   const {
     totalDistance = 0,
     costPerKm = 0,
     efficiency = 0,
-    costPerLiter = 0,
+    // costPerLiter = 0,
     avgDistanceBetweenFills = 0,
   } = fuelStats || {};
 
   const stats = [
+    {
+      label:
+        language === "esp"
+          ? "Total gastado en combustible este año:"
+          : "Total spent this year on fuel:",
+      value: `$${fuelCostThisYear}`,
+    },
     {
       label:
         language === "esp"
@@ -25,10 +35,6 @@ export default function FuelStats() {
     {
       label: language === "esp" ? "Eficiencia:" : "Efficiency:",
       value: `${efficiency.toFixed(2)} ${user?.unitOfMeasure}/L`,
-    },
-    {
-      label: language === "esp" ? "Costo p/Litro:" : "Cost per Liter:",
-      value: `$${costPerLiter.toFixed(2)}`,
     },
     {
       label:
@@ -42,16 +48,17 @@ export default function FuelStats() {
   return (
     <section className="rounded-lg shadow-sm">
       <div className="mb-2 text-[13px] text-yellow-200">
-        {language === "esp" ? "Distancia total:" : "Total Distance"}{" "}
+        {language === "esp" ? "Distancia total:" : "Total Distance:"}{" "}
         <span className="font-bold text-yellow-300">
           {totalDistance.toFixed(2)} {user?.unitOfMeasure}
         </span>
       </div>
+
       <div className="flex flex-wrap items-center justify-center gap-6">
         {stats.map((stat, index) => (
           <div
             key={index}
-            className="825p:flex-col 825p:gap-0 flex items-center gap-2 text-xs shadow-sm"
+            className="flex items-center gap-2 text-xs shadow-sm 825p:flex-col 825p:gap-0"
           >
             <span className="text-[13px] text-blue-200">{stat.label}</span>
             <span className="rounded bg-blue-200 px-1 text-lg font-semibold text-blue-900">
